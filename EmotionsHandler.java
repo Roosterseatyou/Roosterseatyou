1d9cb6311a2f1c4fad0858d5cd6028983b4d9dbd
Roosterseatyou <83042625+Roosterseatyou@users.noreply.github.com>
2021-08-19 21:15:23 -0500

    Update EmotionsHandler.java

--- EmotionsHandler.java
+++ EmotionsHandler.java
@@ -5,7 +5,7 @@ public final class EmotionsHandler extends HumanFunction {
     
 
     @Override
-    public void angerManager(EmotionAngry){
+    public void angerManager(EmotionAngry anger){
     if(anger.getCause == EmotionCause.sibling){
        anger.isUnderstood(true);
        anger.setCooldownTime(Integer.MAX_VALUE);
